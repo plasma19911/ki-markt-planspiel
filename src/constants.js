c67febfc3a022d8ee803cd064e0ccefe5019b530
plasma19911 <308380239+plasma19911@users.noreply.github.com>
2026-08-16 21:26:06 +0200

    Expand ETF universe across major markets sectors factors bonds and commodities

--- src/constants.js
+++ src/constants.js
@@ -4,15 +4,71 @@ export const DEEP_LIMIT = 12;
 export const NEWS_LIMIT = 6;
 export const NEWS_RADAR_BATCH = 8;
 
+// Breites, bewusst kuratiertes ETF-Universum. Die Werte werden mit den 500 groessten Aktien gemeinsam gescannt.
 export const CORE_ETFS = [
- ['SPY','SPDR S&P 500 ETF Trust'],['QQQ','Invesco QQQ Trust'],['VTI','Vanguard Total Stock Market ETF'],
+ // Welt / breite Aktienmaerkte
+ ['SPY','SPDR S&P 500 ETF Trust'],['IVV','iShares Core S&P 500 ETF'],['VOO','Vanguard S&P 500 ETF'],
+ ['VTI','Vanguard Total Stock Market ETF'],['ITOT','iShares Core S&P Total U.S. Stock Market ETF'],['SCHB','Schwab U.S. Broad Market ETF'],
+ ['VT','Vanguard Total World Stock ETF'],['ACWI','iShares MSCI ACWI ETF'],['URTH','iShares MSCI World ETF'],
+ ['RSP','Invesco S&P 500 Equal Weight ETF'],['DIA','SPDR Dow Jones Industrial Average ETF'],
+ ['QQQ','Invesco QQQ Trust'],['QQQM','Invesco NASDAQ 100 ETF'],
  ['VWCE.DE','Vanguard FTSE All-World UCITS ETF USD Accumulating (A2PXXG)'],
  ['EXXT.DE','iShares NASDAQ-100 UCITS ETF (DE) (A0F5UF)'],
- ['IWM','iShares Russell 2000 ETF'],['DIA','SPDR Dow Jones Industrial Average ETF'],['XLK','Technology Select Sector SPDR'],
- ['XLF','Financial Select Sector SPDR'],['XLE','Energy Select Sector SPDR'],['XLV','Health Care Select Sector SPDR'],
- ['GLD','SPDR Gold Shares'],['TLT','iShares 20+ Year Treasury Bond ETF'],['EEM','iShares MSCI Emerging Markets ETF'],
- ['VGK','Vanguard FTSE Europe ETF'],['EWJ','iShares MSCI Japan ETF'],['EWG','iShares MSCI Germany ETF'],
- ['INDA','iShares MSCI India ETF'],['MCHI','iShares MSCI China ETF'],['ARKK','ARK Innovation ETF']
+
+ // Small / Mid Caps
+ ['IWM','iShares Russell 2000 ETF'],['IJR','iShares Core S&P Small-Cap ETF'],['VB','Vanguard Small-Cap ETF'],
+ ['VO','Vanguard Mid-Cap ETF'],['MDY','SPDR S&P MidCap 400 ETF Trust'],
+
+ // International / Europa / Emerging Markets
+ ['VXUS','Vanguard Total International Stock ETF'],['IXUS','iShares Core MSCI Total International Stock ETF'],
+ ['VEA','Vanguard FTSE Developed Markets ETF'],['IEFA','iShares Core MSCI EAFE ETF'],['EFA','iShares MSCI EAFE ETF'],
+ ['VWO','Vanguard FTSE Emerging Markets ETF'],['IEMG','iShares Core MSCI Emerging Markets ETF'],['EEM','iShares MSCI Emerging Markets ETF'],
+ ['EMXC','iShares MSCI Emerging Markets ex China ETF'],['VGK','Vanguard FTSE Europe ETF'],['FEZ','SPDR EURO STOXX 50 ETF'],
+ ['EZU','iShares MSCI Eurozone ETF'],['EWG','iShares MSCI Germany ETF'],['EWU','iShares MSCI United Kingdom ETF'],
+ ['EWQ','iShares MSCI France ETF'],['EWI','iShares MSCI Italy ETF'],['EWP','iShares MSCI Spain ETF'],['EWL','iShares MSCI Switzerland ETF'],
+ ['EWN','iShares MSCI Netherlands ETF'],['EWD','iShares MSCI Sweden ETF'],
+
+ // Asien / Laender
+ ['EWJ','iShares MSCI Japan ETF'],['DXJ','WisdomTree Japan Hedged Equity Fund'],['MCHI','iShares MSCI China ETF'],
+ ['FXI','iShares China Large-Cap ETF'],['KWEB','KraneShares CSI China Internet ETF'],['ASHR','Xtrackers Harvest CSI 300 China A-Shares ETF'],
+ ['INDA','iShares MSCI India ETF'],['EPI','WisdomTree India Earnings Fund'],['EWY','iShares MSCI South Korea ETF'],
+ ['EWT','iShares MSCI Taiwan ETF'],['EWA','iShares MSCI Australia ETF'],['EWC','iShares MSCI Canada ETF'],
+ ['EWZ','iShares MSCI Brazil ETF'],['EWW','iShares MSCI Mexico ETF'],['EZA','iShares MSCI South Africa ETF'],
+
+ // US-Sektoren
+ ['XLK','Technology Select Sector SPDR Fund'],['XLF','Financial Select Sector SPDR Fund'],['XLE','Energy Select Sector SPDR Fund'],
+ ['XLV','Health Care Select Sector SPDR Fund'],['XLI','Industrial Select Sector SPDR Fund'],['XLY','Consumer Discretionary Select Sector SPDR Fund'],
+ ['XLP','Consumer Staples Select Sector SPDR Fund'],['XLU','Utilities Select Sector SPDR Fund'],['XLB','Materials Select Sector SPDR Fund'],
+ ['XLRE','Real Estate Select Sector SPDR Fund'],['XLC','Communication Services Select Sector SPDR Fund'],
+ ['VGT','Vanguard Information Technology ETF'],['SMH','VanEck Semiconductor ETF'],['SOXX','iShares Semiconductor ETF'],
+ ['XBI','SPDR S&P Biotech ETF'],['IBB','iShares Biotechnology ETF'],['XHB','SPDR S&P Homebuilders ETF'],
+ ['ITB','iShares U.S. Home Construction ETF'],['KRE','SPDR S&P Regional Banking ETF'],['KBE','SPDR S&P Bank ETF'],
+ ['OIH','VanEck Oil Services ETF'],['XOP','SPDR S&P Oil & Gas Exploration & Production ETF'],
+
+ // Faktoren / Dividenden / Stil
+ ['VUG','Vanguard Growth ETF'],['VTV','Vanguard Value ETF'],['IWF','iShares Russell 1000 Growth ETF'],['IWD','iShares Russell 1000 Value ETF'],
+ ['QUAL','iShares MSCI USA Quality Factor ETF'],['MTUM','iShares MSCI USA Momentum Factor ETF'],['USMV','iShares MSCI USA Min Vol Factor ETF'],
+ ['SPLV','Invesco S&P 500 Low Volatility ETF'],['VLUE','iShares MSCI USA Value Factor ETF'],['SPHQ','Invesco S&P 500 Quality ETF'],
+ ['SCHD','Schwab U.S. Dividend Equity ETF'],['VIG','Vanguard Dividend Appreciation ETF'],['DVY','iShares Select Dividend ETF'],
+ ['HDV','iShares Core High Dividend ETF'],['NOBL','ProShares S&P 500 Dividend Aristocrats ETF'],
+
+ // Anleihen / Cash / Inflation
+ ['BND','Vanguard Total Bond Market ETF'],['AGG','iShares Core U.S. Aggregate Bond ETF'],['BNDX','Vanguard Total International Bond ETF'],
+ ['TLT','iShares 20+ Year Treasury Bond ETF'],['IEF','iShares 7-10 Year Treasury Bond ETF'],['SHY','iShares 1-3 Year Treasury Bond ETF'],
+ ['SGOV','iShares 0-3 Month Treasury Bond ETF'],['BIL','SPDR Bloomberg 1-3 Month T-Bill ETF'],['VGSH','Vanguard Short-Term Treasury ETF'],
+ ['VGIT','Vanguard Intermediate-Term Treasury ETF'],['LQD','iShares iBoxx Investment Grade Corporate Bond ETF'],['VCIT','Vanguard Intermediate-Term Corporate Bond ETF'],
+ ['HYG','iShares iBoxx High Yield Corporate Bond ETF'],['JNK','SPDR Bloomberg High Yield Bond ETF'],['TIP','iShares TIPS Bond ETF'],
+ ['VTIP','Vanguard Short-Term Inflation-Protected Securities ETF'],['MUB','iShares National Muni Bond ETF'],
+
+ // Immobilien / Rohstoffe / Edelmetalle
+ ['VNQ','Vanguard Real Estate ETF'],['IYR','iShares U.S. Real Estate ETF'],['REET','iShares Global REIT ETF'],
+ ['GLD','SPDR Gold Shares'],['IAU','iShares Gold Trust'],['SLV','iShares Silver Trust'],['DBC','Invesco DB Commodity Index Tracking Fund'],
+ ['PDBC','Invesco Optimum Yield Diversified Commodity Strategy No K-1 ETF'],['USO','United States Oil Fund'],['UNG','United States Natural Gas Fund'],
+
+ // Zukunftsthemen / Spezialthemen
+ ['ARKK','ARK Innovation ETF'],['BOTZ','Global X Robotics & Artificial Intelligence ETF'],['ROBO','ROBO Global Robotics and Automation Index ETF'],
+ ['TAN','Invesco Solar ETF'],['ICLN','iShares Global Clean Energy ETF'],['LIT','Global X Lithium & Battery Tech ETF'],
+ ['COPX','Global X Copper Miners ETF'],['URA','Global X Uranium ETF'],['CIBR','First Trust Nasdaq Cybersecurity ETF'],['HACK','ETFMG Prime Cyber Security ETF']
 ].map(([symbol,name])=>({symbol,name,type:'ETF',leverage:1}));
 
 export const LEVERAGED_ETFS = [
