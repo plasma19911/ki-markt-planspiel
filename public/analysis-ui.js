81d8a7775f8af2e86fdd414246054aae1d1c19ca
plasma19911 <308380239+plasma19911@users.noreply.github.com>
2026-08-27 12:34:21 +0200

    Load live stock news dashboard module

--- public/analysis-ui.js
+++ public/analysis-ui.js
@@ -2,6 +2,7 @@ import './quota-guard.js';
 import './investment-ui.js?v=20260819-1140';
 import './agm-calendar-ui.js?v=20260820-1447';
 import './daytrade-largecap-ui.js?v=20260821-1135';
+import './news-learning-ui.js?v=20260827-1223';
 
 // Die sichtbare Oberfläche wird absichtlich nicht mehr per JavaScript in neue
 // Hauptbereiche umgebaut. V30.0 sortiert die vorhandenen Depotkarten nach Chance
